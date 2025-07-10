fa07065e8df488bfcc7b0048347eb245cb811f00
yashaswip333 <yashaswip@google.com>
2025-07-10 23:32:11 +0530

    Create test4.tf

---
+++ test4.tf
@@ -0,0 +1,8 @@
+resource "google_organization_iam_binding" "securitycenter_admin" {
+  org_id = "274706244275"
+  role = "roles/securitycenter.admin"
+  members = [
+    "user:pkhushalani@google.com",
+    "user: aditv@google.com"
+  ]
+}
