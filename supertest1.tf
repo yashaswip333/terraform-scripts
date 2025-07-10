cf469a6ed0d0374c0125ee9d0db89447a07953b8
yashaswip333 <yashaswip@google.com>
2025-07-10 23:26:14 +0530

    Create supertest1.tf

---
+++ supertest1.tf
@@ -0,0 +1,107 @@
+resource "google_organization_iam_binding" "chronicle_api_admin" {
+  org_id = "274706244275"
+  role   = "roles/chronicle.admin" 
+  members = [
+    "user:foo@example.com"
+    "user:pkhushalani@google.com",
+  ]
+}
+
+resource "google_organization_iam_binding" "latchkey_hierarchy_traversal" {
+  org_id = "274706244275"
+  role   = "organizations/274706244275/roles/latchkey_hierarchy_traversal" 
+  members = [
+    "user:foo@example.com"
+  ]
+}
+
+
+resource "google_organization_iam_member" "chronicle_service_admin" {
+  org_id = "274706244275"
+  role   = "roles/chroniclesm.admin" 
+  member = "user:pkhushalani@google.com"
+}
+
+resource "google_organization_iam_member" "chronicle_service_admin" {
+  org_id = "274706244275"
+  role   = "roles/chroniclesm.admin" 
+  member = "user:foo@example.com"
+}
+
+resource "google_organization_iam_binding" "chronicle_soar_admin" {
+  org_id = "274706244275"
+  role   = "roles/chronicle.soarAdmin" 
+  members = [
+    "user:pkhushalani@google.com",
+  ]
+}
+
+resource "google_organization_iam_member" "cloud_asset_owner" {
+  org_id = "274706244275"
+  role   = "roles/cloudasset.owner"
+  member = "user:pkhushalani@google.com"
+}
+
+resource "google_organization_iam_member" "create_service_accounts" {
+  org_id = "274706244275"
+  role   = "roles/iam.serviceAccountCreator"
+  member = "user:pkhushalani@google.com"
+}
+
+resource "google_organization_iam_member" "folder_iam_admin" {
+  org_id = "274706244275"
+  role   = "roles/resourcemanager.folderIamAdmin"
+  member = "user:pkhushalani@google.com"
+}
+
+resource "google_organization_iam_member" "iam_recommender_admin" {
+  org_id = "274706244275"
+  role   = "roles/recommender.iamAdmin"
+  member = "user:pkhushalani@google.com"
+}
+
+resource "google_organization_iam_binding" "organization_administrator" {
+  org_id = "274706244275"
+  role   = "roles/resourcemanager.organizationAdmin"
+  members = [
+    "user:pkhushalani@google.com",
+  ]
+}
+
+resource "google_organization_iam_member" "organization_role_administrator" {
+  org_id = "274706244275"
+  role   = "roles/iam.organizationRoleAdmin"
+  member = "user:pkhushalani@google.com"
+}
+
+resource "google_organization_iam_binding" "owner" {
+  org_id = "274706244275"
+  role   = "roles/owner"
+  members = [
+    "user:pkhushalani@google.com",
+  ]
+}
+
+resource "google_organization_iam_member" "project_creator" {
+  org_id = "274706244275"
+  role   = "roles/resourcemanager.projectCreator"
+  member = "user:pkhushalani@google.com"
+}
+
+resource "google_organization_iam_member" "project_iam_admin" {
+  org_id = "274706244275"
+  role   = "roles/resourcemanager.projectIamAdmin"
+  member = "user:pkhushalani@google.com"
+}
+
+resource "google_organization_iam_member" "security_center_admin" {
+  org_id = "274706244275"
+  role   = "roles/securitycenter.admin"
+  member = "user:pkhushalani@google.com"
+}
+
+resource "google_organization_iam_member" "iam_security_admin" {
+  org_id = "274706244275"
+  role   = "roles/iam.securityAdmin"
+  member = "user:pkhushalani@google.com"
+}
