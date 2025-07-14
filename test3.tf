62682f70c10ad9a2e6119dd6153e68bc7b23f136
yashaswip333 <yashaswip@google.com>
2025-07-14 14:52:51 +0530

    Update test3.tf

--- test3.tf
+++ test3.tf
@@ -2,15 +2,6 @@ resource "google_organization_iam_binding" "securitycenter_admin" {
   org_id = "274706244275"
   role   = "roles/securitycenter.admin"
   members = [
-    "user:aditv@google.com",
     "user:pkhushalani@google.com",
-  ]
-}
-
-resource "google_organization_iam_binding" "latchkey_hierarchy_traversal" {
-  org_id = "274706244275"
-  role   = "organizations/274706244275/roles/latchkey_hierarchy_traversal"
-  members = [
-    "user:aditv@google.com",
   ]
 }
